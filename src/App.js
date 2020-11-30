6c17643e66cecea0fcbada3406e0b6ba4a96cfe6
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-11-30 11:42:25 +0530

    SearchBar using WikiPedia API Built Success

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from 'react';
-import Accordion from './components/Accordion';
-
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
-  },
-];
-
-
-/*
-Line 19:1:  Assign arrow function to a variable before exporting as module default
-  import/no-anonymous-default-export
-export default () => {
-  */
+import Search from './components/SearchWiki';
 
 const App=()=>{
   return (
     <div>
-      <Accordion items={items} />
+      <Search/>
     </div>
   );
 };
