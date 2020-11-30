44299411e094d7572618a61efad829dd2f0835a4
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-11-30 11:22:00 +0530

    Accordion Widget Build Successful

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
 
 
 /*
@@ -10,7 +26,7 @@ export default () => {
 const App=()=>{
   return (
     <div>
-      WELCOME
+      <Accordion items={items} />
     </div>
   );
 };
