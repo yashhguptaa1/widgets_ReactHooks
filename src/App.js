668cffa55fe95ce61db0acf94bd87a6445b3607c
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-12-03 12:15:49 +0530

    Using useState in Dropdown widget

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import Search from './components/SearchWiki';
+import React ,{ useState } from 'react';
+//import Accordion from './components/Accordion';
+//import Search from './components/SearchWiki';
+import Dropdown from './components/Dropdown';
+
+const itemsAccordion = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
+
+
+const optionsDropdown = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue',
+  },
+];
+
 
 const App=()=>{
+  
+  const [selected, setSelected]=useState(optionsDropdown[0]);
+
   return (
     <div>
-      <Search/>
+      {
+      /*<Accordion items={itemsAccordion}>
+      /*<Search/>*/
+        
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={optionsDropdown}
+        />
+      }
     </div>
   );
 };
